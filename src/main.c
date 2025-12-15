e5203b97bc0d1ef28ea9d1f625782a66404ad2b6
Davirain <davirain.yin@gmail.com>
2025-12-15 22:03:49 +0800

    refactor count

--- src/main.c
+++ src/main.c
@@ -4,11 +4,11 @@
 int main() {
     int nc = 0;
     int nl = 0;
-    int c = 0;
-    for(;(c = getchar()) != EOF; nc += 1) {
+    for(int c = 0; c != EOF; nc += 1) {
         if (c == '\n') {
             nl += 1;
         }
+        c = getchar();
     }
     printf("\nINPUT NUMBERIS: %ld\n", nc);
     printf("\nINPUT LINEIS: %ld\n", nl);

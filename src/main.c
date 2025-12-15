ac17a8d737ebd375addcac8c94c8835ac25f57f7
Davirain <davirain.yin@gmail.com>
2025-12-15 22:02:07 +0800

    add count line

--- src/main.c
+++ src/main.c
@@ -3,9 +3,14 @@
 
 int main() {
     int nc = 0;
-    while (getchar() != EOF) {
-        nc += 1;
+    int nl = 0;
+    int c = 0;
+    for(;(c = getchar()) != EOF; nc += 1) {
+        if (c == '\n') {
+            nl += 1;
+        }
     }
     printf("\nINPUT NUMBERIS: %ld\n", nc);
+    printf("\nINPUT LINEIS: %ld\n", nl);
     return 0;
 }

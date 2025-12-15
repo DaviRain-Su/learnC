5d3b3ce9835ffdfb8f3c1a6f209f9adc74ef16e6
Davirain <davirain.yin@gmail.com>
2025-12-15 21:45:25 +0800

    echo

--- src/main.c
+++ src/main.c
@@ -1,20 +1,11 @@
 #include <stdio.h>
 #include "foo.h"
 
-#define MAX_FAHRENHEIT 300
-#define STEP 20
-
-// 函数原型声明
-void display_fahrenheit_convert_with_celsius(int fahrenheit);
-
 int main() {
-    for (int fahr = 0; fahr <= MAX_FAHRENHEIT; fahr += STEP) {
-        display_fahrenheit_convert_with_celsius(fahr);
+    int c = 0;
+    while (c != EOF) {
+        putchar(c);
+        c = getchar();
     }
     return 0;
-}
-
-void display_fahrenheit_convert_with_celsius(int fahrenheit) {
-    float celsius = (fahrenheit - 32.0) * 5.0 / 9.0;
-    printf("%d°F is %.2f°C\n", fahrenheit, celsius);
 }

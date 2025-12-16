07c2360ccbacd54b746a266e08f359b2b24b423d
Davirain <davirain.yin@gmail.com>
2025-12-16 16:11:49 +0800

    Add count digits num

--- src/main.c
+++ src/main.c
@@ -3,11 +3,15 @@
 #define IN 1
 #define OUT 0
 
+// 统计数字，空白符及其他字符出现的次数
 int main() {
     int nc = 0;
     int nl = 0;
     int nw = 0;
     int state = OUT;
+    int ndigit[10] = {0};
+    int nwhite = 0;
+    int nother = 0;
 
     for(int c = 0; c != EOF; nc += 1) {
         if (c == '\n') {
@@ -15,8 +19,13 @@ int main() {
         }
         switch (state) {
             case IN: {
-                if (c == ' ' || c == '\t' || c == '\n') {
+                if (c >= '0' && c <= '9') {
+                    ndigit[c - '0'] += 1;
+                } else if (c == ' ' || c == '\t' || c == '\n') {
+                    nwhite += 1;
                     state = OUT;
+                } else {
+                    nother += 1;
                 }
                 break;
             }
@@ -31,5 +40,18 @@ int main() {
     printf("\nINPUT NUMBERIS: %ld\n", nc);
     printf("\nINPUT LINEIS: %ld\n", nl);
     printf("\nINPUT WORDIS: %ld\n", nw);
+
+    printf("\nDIGITS:\n");
+    for (int i = 1; i <= 10; i++) {
+        printf("%3d ",i);
+    }
+    printf("\n");
+    for (int i = 0; i < 10; i++) {
+        printf("%3d ",ndigit[i]);
+    }
+    printf("\n");
+    printf("\nWHITE SPACE: %d\n", nwhite);
+    printf("\nOTHER CHARACTERS: %d\n", nother);
+
     return 0;
 }

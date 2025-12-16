99635de4c2dbf9254772026ed9577a3116d46461
DaviRain-Su <davirain.yin@gmail.com>
2025-12-16 11:17:22 +0800

    refactor go isValid

--- go/leetcode.go
+++ go/leetcode.go
@@ -1,23 +1,17 @@
 package leetcode
 
 func isValid(s string) bool {
-	stack := []rune{}
+	stack := []int32{}
 	for _, c := range s {
 		switch c {
-		case '(', '{', '[':
-			stack = append(stack, c)
-		case ')':
-			if len(stack) == 0 || stack[len(stack)-1] != '(' {
-				return false
-			}
-			stack = stack[:len(stack)-1]
-		case '}':
-			if len(stack) == 0 || stack[len(stack)-1] != '{' {
-				return false
-			}
-			stack = stack[:len(stack)-1]
-		case ']':
-			if len(stack) == 0 || stack[len(stack)-1] != '[' {
+		case '(':
+			stack = append(stack, ')')
+		case '{':
+			stack = append(stack, '}')
+		case '[':
+			stack = append(stack, ']')
+		case ')', '}', ']':
+			if len(stack) == 0 || stack[len(stack)-1] != c {
 				return false
 			}
 			stack = stack[:len(stack)-1]
